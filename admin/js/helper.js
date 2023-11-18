ba43fa8a9fc88899db746359692f6fb9b465a621
simanto-sroy <sroy38703@gmail.com>
2023-11-19 05:39:35 +0600

    tinygone-frontend

---
+++ admin/js/helper.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#summernote').summernote({
+        tabsize: 2,
+        height: 300
+    });
+    
+});
+
+$('.dropify').dropify({
+    tpl: {
+        wrap:            '<div class="dropify-wrapper"></div>',
+        loader:          '<div class="dropify-loader"></div>',
+        message:         '<div class="dropify-message"><span class="file-icon" /> <p>{{ default }}</p></div>',
+        preview:         '<div class="dropify-preview"><span class="dropify-render"></span><div class="dropify-infos"><div class="dropify-infos-inner"><p class="dropify-infos-message">{{ replace }}</p></div></div></div>',
+        filename:        '<p class="dropify-filename"><span class="file-icon"></span> <span class="dropify-filename-inner"></span></p>',
+        clearButton:     '<button type="button" class="dropify-clear">{{ remove }}</button>',
+        errorLine:       '<p class="dropify-error">{{ error }}</p>',
+        errorsContainer: '<div class="dropify-errors-container"><ul></ul></div>'
+    }
+});
+
+$(document).ready(function() {
+    $('.select-tags').select2();
+});
